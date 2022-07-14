6c9190e5c5089fb667d5d2dbc1f05d43eae4bf77
Marcin Niemiec <marcinniemiec87@gmail.com>
2022-07-14 20:48:26 +0200

    The longest word game has been created without css part

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,15 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
-    @letters = ('A'..'Z').to_a.sample(10)
+    @letters = 10.times.map { [*('A'..'Z')].sample }
   end
 
   def score
-    # params[:answer]
-    raise
+    @characters = params[:answer].upcase.chars
+    @letters = new
+    @letters.each { |el| @characters.delete_at(@characters.index(el)) if @characters.index(el) }
+    @word = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{params[:answer]}").read)
   end
 end
